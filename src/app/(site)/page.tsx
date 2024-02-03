89edc6a0db54d51c06f1262599fc6e78731e94f3
dirtymalikjin9k <hopemalik0512@gmail.com>
2024-02-03 10:08:40 -0500

    Initialize Trial Task Project

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+export default function Home() {
+	return (
+		<main>
+			<div className="bg-[#0D121F] px-[100px] text-white">
+				<h1>Hello, Trial Task</h1>
+			</div>
+		</main>
+	);
+}
